57c56c506e0541ad498a609a3cd41218b86c37b8
Alexander Paul <info@questgig.com>
2023-10-30 10:29:09 +0100

    Updated app.ts: Added Error Handling and Proper Env Prasing

--- fake-src/app.ts
+++ fake-src/app.ts
@@ -1,18 +1,24 @@
-import express from 'express';
+import express, { Express } from 'express';
+import { Router } from 'express';
 import { authRouter } from './routes/auth';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 3000;
-
+const app: Express = express();
+const port: number = parseInt(process.env.PORT as string, 10) || 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
 // Use the defined router
 app.use('/auth', authRouter);
+
+// Error handling middleware
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 // Start the Express server
 app.listen(port, () => {
